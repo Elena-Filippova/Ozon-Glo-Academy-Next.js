112cefb98a4659bd01d4c58af7da0f00007c9e38
Elena-Filippova <157601139+Elena-Filippova@users.noreply.github.com>
2025-11-19 14:12:19 +0300

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import localFont from 'next/font/local'
+import Header from './ui/header'
+import Cart from './ui/cart'
+import './scss/style.scss'
+
+const GTEestiProText = localFont({
+  src: [
+    {
+      path: "./fonts/EestiRegular.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/EestiBold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        ></link>
+      </head>
+      <body className={GTEestiProText.className}>
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Cart />
+      </body>
+    </html>
+  )
+}
